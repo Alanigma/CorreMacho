37e8036be1f7134d9fae1d1cee2e1dfd123fed1c
Alan Sol Alves Dias <alanlua2020@hotmail.com>
2025-06-04 21:49:33 -0300

    Fix pause close with esc

--- CorreMachoProject/Assets/Prefabs/pause/PauseHUD.cs
+++ CorreMachoProject/Assets/Prefabs/pause/PauseHUD.cs
@@ -10,41 +10,28 @@ public class PauseHUD : MonoBehaviour
     private void OnEnable()
     {
 
-        //ObserverPause.OnPause += PauseGame;
+        GameSettingsObserver.OnPauseButtonClick += PauseGame;
 
     }
 
     private void OnDisable()
     {
 
-        //ObserverPause.OnPause -= PauseGame;
-        GameSettingsObserver.PauseButtonClick();
+        GameSettingsObserver.OnPauseButtonClick -= PauseGame;
 
     }
 
     public void CallPause() => ObserverPause.CallPause();
 
+    public void CallPauseClick()
+    {
+        GameSettingsObserver.PauseButtonClick();
+    }
+
     private void PauseGame()
     {
 
-        if (Time.timeScale == 0 && !m_IsPaused) return;
-
-        m_IsPaused = !m_IsPaused;
-
-        if (m_IsPaused)
-        {
-            m_PauseCanvas.alpha = 1;
-            m_PauseCanvas.interactable = true;
-            m_PauseCanvas.blocksRaycasts = true;
-            Time.timeScale = 0;
-        }
-        else
-        {
-            m_PauseCanvas.alpha = 0;
-            m_PauseCanvas.interactable = false;
-            m_PauseCanvas.blocksRaycasts = false;
-            Time.timeScale = 1;
-        }
+        //
 
     }
 
